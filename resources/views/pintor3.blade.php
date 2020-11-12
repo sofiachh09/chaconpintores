5f3eb25a727213bd641e636ed997bb34831c840e
sofiachh09 <64874871+sofiachh09@users.noreply.github.com>
2020-11-12 00:10:17 -0700

    Add files via upload

---
+++ resources/views/pintor3.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.master')
+
+@section('header')
+@stop
+@section('navbar')
+    @parent
+@stop
+@section('content')
+    <header>
+       <center><h1>Vincent Van Goghx</h1></center> 
+    </header>
+    
+    
+    <h4>Los girsasoles</h4>
+    <p>Más que un único cuadro, se trata de una serie de girasoles realizados en 1888 bastante similares entre sí. El color amarillo y la flor son elementos constantes en la obra del artista.</p>
+    <img src="imagenes\11.jpg" alt=""  width="450px" height="390" width="260">
+   <br>
+    <br>
+   
+    <h4>La noche estrellada</h4>
+    <p>El pintor hizo alrededor de 43 autorretratos. Éste lo realizó en el invierno de 1889, medio año antes de su muerte. Se puede apreciar la textura en ondas, característica fundamental de la obra de Van Gogh.</p>
+    <img src="imagenes\12.jpg" alt=""  height="390" width="260">
+    <h4>El Autorretrato</h4>
+    <p>Stella, de 17 años, está ingresada en un hospital porque padece fibrosis quística. Su monótona existencia cambia cuando llega Will, un adolescente con la misma dolencia. Sin embargo, las normas del hospital prohíben el contacto entre ellos.</p>
+    <img src="imagenes\13.jpg" alt="" height="390" width="260">
+    
+    <h4>Autorretrato con la oveja vendada </h4>
+    <p>Es un cuadro inacabado atribuido al pintor renacentista italiano Miguel Ángel. La datación es alrededor de 1500-1501.
+        El centro del cuadro representa a Jesucristo llevado al sepulcro. El hombre con barba que está tras él es José de Arimatea, el hombre que dio su tumba para la sepultura de Cristo. La figura de la izquierda es probablemente san Juan, con una larga túnica roja, mientras que arrodillada a sus pies estaría María Magdalena.
+    </p>
+    <img src="imagenes\14.jpg" alt="" height="390" width="260">
+    <br>
+    
+    <h4>El dormitorio en Arlés </h4>
+    <p>El artista capturó su lugar de estancia en la ciudad de Arlés en tres cuadros casi idénticos. La primera versión fue realizada en 1888 y las otras dos al año siguiente.</p>
+    <img src="imagenes\15.jpg" alt="" height="390" width="260">
+@stop
+@section('footer')
+    @parent
+@stop
---
+++ resources/views/pintor4.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.master')
+
+@section('header')
+@stop
+@section('navbar')
+     @parent
+@stop
+@section('content')
+
+
+<header>
+    <center><h1>Edvard Munch</h1></center>
+</header>
+
+
+<h4>Ansiedad</h4>
+<p>Ansiedad (en noruego, Angst: ) es un óleo sobre lienzo pintado por el artista expresionista Edvard Munch en 1894. Está actualmente en el Museo Munch de Oslo, Noruega.</p>
+<img id="" src="imagenes\16.jpg" alt="" height="390" width="260">
+
+<h4>Amor y Dolor (Vampiro)</h4>
+<p>Amor y Dolor es una pintura de Edvard Munch, también a menudo denominada Vampiro, aunque no por Munch.</p>
+<img id="" src="imagenes\17.jpg" alt="" height="390" width="260">
+
+<h4>Pubertad</h4>
+<p>Pubertad (en noruego, Pubertet: Pubertet) es una pintura de 1894–95 de Edvard Munch, pintor noruego destacado en el arte expresionista. Pubertad también fue reproducida en litografía y aguafuerte por Munch.</p>
+<img id=""src="imagenes\18.jpg" alt="" height="390" width="260">
+<br>
+
+
+<h4>La danza de la vida</h4>
+<p>Estoy bailando con mi verdadero amor: con un recuerdo de ella. Una rubia sonriente entra y desea coger la flor del amor, pero no le está permitido. </p>
+<img id="" src="imagenes\19.jpg" alt="" height="380" width="260">
+<br>
+
+
+<h4>Cenizas</h4>
+<p>Cuenta una historia mediante imágenes, donde el título "Cenizas" significa lo que queda entre los amantes cuando termina el fuego de la pasión.
+</p>
+<img id=""src="imagenes\20.jpg" alt="" height="380" width="260">
+
+
+
+@stop
+@section('footer')
+    @parent
+@stop
